06a50286799f9115c8bee80b4449e217d351a409
Neko-prototype <weiyanghan70@gmail.com>
2025-03-27 10:04:22 +0800

    折線圖

--- test1.py
+++ test1.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-BM_data=pd.read_csv("output-data/big-mac-2025-01-01.xls")  
+BM_data=pd.read_csv("output-data/2025.csv")  
 print(BM_data)
 x=BM_data['Country']
-y=BM_data['local_price']
+y=BM_data['dollar_price']
+plt.xlabel("Country")
+plt.ylabel("dollar_price")
+plt.plot(x,y)
+plt.show()
