09bf7ae1ceb43901ec4ddc9d80cebc7983e7aa30
Neko-prototype <weiyanghan70@gmail.com>
2025-03-27 09:31:39 +0800

    刪檔案

--- test1.py
+++ test1.py
@@ -1,3 +1,6 @@
-print("big mac")
-print("py") 
-print("neko")
+import pandas as pd
+import matplotlib.pyplot as plt
+BM_data=pd.read_csv("output-data/big-mac-2025-01-01.xls")  
+print(BM_data)
+x=BM_data['Country']
+y=BM_data['local_price']
